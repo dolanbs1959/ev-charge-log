af944936e4358db4a085e110fc18a8059fbb3c33
Barry Dolan <dolanbs1959@gmail.com>
2026-02-01 17:29:31 -0800

    new push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChargeService } from './services/charge.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'charge-logger';
+  currentKwh: number | null = null;
+  chargeRate: number = 0.20;
+  totals: any = null;
+  chargeDate: string = new Date().toISOString().split('T')[0]; 
+  rangeStart: string = '';
+  rangeEnd: string = '';
+  showTotals: boolean = false;
+  constructor(private chargeService: ChargeService) {}
+
+submitCharge() {
+  if (this.currentKwh && this.chargeDate) {
+    const [year, month, day] = this.chargeDate.split('-');
+    const formattedDate = `${month}/${day}/${year}`;
+    
+    // We send '0' or null for cost because we aren't calculating it yet
+    this.chargeService.logCharge(this.currentKwh, 0, formattedDate).subscribe(() => {
+      alert('⚡ Charge Logged!');
+      this.currentKwh = null;
+    });
+  }
+}
+
+  fetchTotals() {
+    // Now passing BOTH start and end dates to the service
+    this.chargeService.getHistory(this.rangeStart, this.rangeEnd).subscribe((data) => {
+      this.totals = data;
+    });
+  }
 }
